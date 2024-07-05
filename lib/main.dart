0a28f97264bfb0e524484646faf1f2c41ebe189d
yabsiraditya <yabsir662@gmail.com>
2024-07-05 23:03:19 +0700

    day 1 learn flutter

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(new MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
@@ -10,22 +8,24 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text("Aplikasi Hello World"),
+          title: Text("Latihan Row dan Column"),
           backgroundColor: Colors.lime,
         ),
-        body: Center(
-          child: Container(
-              color: Colors.lime,
-              width: 150,
-              height: 150,
-              child: Text(
-                "Melatih Kemampuan Belajar Flutter Saya bersama Pak Erico.",
-                style: TextStyle(
-                    color: Colors.red,
-                    fontStyle: FontStyle.italic,
-                    fontWeight: FontWeight.w700,
-                    fontSize: 20),
-              )),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Text("Text 1"),
+            Text("Text 2"),
+            Text("Text 3"),
+            Row(
+              children: <Widget>[
+                Text("Text 4"),
+                Text("Text 5"),
+                Text("Text 6")
+              ],
+            )
+          ],
         ),
       ),
     );
