d27bed973bfc7dee79c39ae5e618c1d0ba571e16
yabsiraditya <yabsir662@gmail.com>
2024-07-08 08:48:13 +0700

    day 3 learn-flutter State Full Widget

--- lib/main.dart
+++ lib/main.dart
@@ -2,26 +2,50 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  int number = 0;
+
+// Tombol Tambah
+  void tombolTambah() {
+    setState(() {
+      number = number + 1;
+    });
+  }
+
+  void tombolKurang() {
+    setState(() {
+      number = number - 1;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text("Latihan Row dan Column"),
+          title: Text("Stateful Widget Demo"),
           backgroundColor: Colors.lime,
         ),
-        body: Container(
-          color: Colors.red,
-          margin: EdgeInsets.fromLTRB(10, 15, 20, 25),
-          padding: EdgeInsets.only(bottom: 20, top: 20),
-          child: Container(
-            margin: EdgeInsets.all(10),
-            decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(20),
-                gradient: LinearGradient(
-                    begin: Alignment.bottomCenter,
-                    colors: <Color>[Colors.green, Colors.amber])),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                number.toString(),
+                style: TextStyle(fontSize: 10 + number.toDouble()),
+              ),
+              ElevatedButton(
+                  onPressed: tombolTambah, child: Text("Tambah Bilangan")),
+              ElevatedButton(
+                  onPressed: tombolKurang, child: Text("Kurang Bilangan"))
+            ],
           ),
         ),
       ),
