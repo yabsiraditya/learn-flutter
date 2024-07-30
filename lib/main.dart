2a58324e767be7ae3e90253597559693d41d8c01
yabsiraditya <yabsir662@gmail.com>
2024-07-30 14:53:10 +0700

    day 6 learn-flutter App Bar Gradient

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:learn_flutter/loginPage.dart';
 
 void main() => runApp(MyApp());
 
@@ -9,7 +8,33 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: LoginPage(),
+      home: Scaffold(
+        appBar: AppBar(
+          leading: Icon(
+            Icons.adb,
+            color: Colors.white,
+          ),
+          title: Text(
+            "AppBar Example",
+            style: TextStyle(color: Colors.white),
+          ),
+          actions: <Widget>[
+            IconButton(onPressed: () {}, icon: Icon(Icons.settings)),
+            IconButton(onPressed: () {}, icon: Icon(Icons.exit_to_app)),
+          ],
+          flexibleSpace: Container(
+            decoration: BoxDecoration(
+                gradient: LinearGradient(
+                    colors: [Color(0xff0096ff), Color(0xff6610f2)],
+                    begin: FractionalOffset.topLeft,
+                    end: FractionalOffset.bottomRight),
+                image: DecorationImage(
+                    image: AssetImage("images/doted.png"),
+                    fit: BoxFit.none,
+                    repeat: ImageRepeat.repeat)),
+          ),
+        ),
+      ),
     );
   }
 }
