8c29c46eef340beafb81e0e555e325640aeba19a
yabsiraditya <yabsir662@gmail.com>
2024-08-02 01:49:00 +0700

    day 7 learn-flutter Opacity Custom Card

--- lib/main.dart
+++ lib/main.dart
@@ -4,56 +4,12 @@ import 'package:learn_flutter/mainPage.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("Latihan Membuat button"),
-          backgroundColor: Colors.lime,
-        ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: <Widget>[
-              ElevatedButton(
-                onPressed: () {},
-                child: Text("Elevated Button"),
-                style: ElevatedButton.styleFrom(backgroundColor: Colors.amber),
-              ),
-              Material(
-                elevation: 2,
-                borderRadius: BorderRadius.circular(20),
-                child: Container(
-                  width: 150,
-                  height: 40,
-                  decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(20),
-                      gradient: LinearGradient(
-                          colors: [Colors.purple, Colors.pink],
-                          begin: Alignment.topCenter,
-                          end: Alignment.bottomCenter)),
-                  child: Material(
-                    borderRadius: BorderRadius.circular(20),
-                    color: Colors.transparent,
-                    child: InkWell(
-                      splashColor: Colors.amber,
-                      borderRadius: BorderRadius.circular(20),
-                      onTap: () {},
-                      child: Center(
-                          child: Text(
-                        "MyButton",
-                        style: TextStyle(
-                            color: Colors.white, fontWeight: FontWeight.w600),
-                      )),
-                    ),
-                  ),
-                ),
-              ),
-            ],
-          ),
-        ),
-      ),
+      home: MainPage(),
     );
   }
 }
--- lib/mainPage.dart
+++ lib/mainPage.dart
@@ -5,36 +5,131 @@ class MainPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Latihan Media Query"),
-        backgroundColor: Colors.lime,
+        backgroundColor: Colors.black87,
+        title: Text(
+          "Custom Card",
+          style: TextStyle(color: Colors.white),
+        ),
       ),
-      body: (MediaQuery.of(context).orientation == Orientation.portrait)
-          ? Column(
-              children: generateContainer,
-            )
-          : Row(
-              children: generateContainer,
+      body: Stack(
+        children: <Widget>[
+          Container(
+            decoration: BoxDecoration(
+                gradient: LinearGradient(
+                    colors: [Color(0xfffe5788), Color(0xfff56d5d)],
+                    begin: Alignment.topCenter,
+                    end: Alignment.bottomCenter)),
+          ),
+          Center(
+            child: SizedBox(
+              width: MediaQuery.of(context).size.width * 0.8,
+              height: MediaQuery.of(context).size.height * 0.7,
+              child: Card(
+                elevation: 10,
+                child: Stack(
+                  children: <Widget>[
+                    Opacity(
+                      opacity: 0.7,
+                      child: Container(
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(4),
+                            image: DecorationImage(
+                                image: AssetImage("images/doted.jpg"),
+                                fit: BoxFit.cover)),
+                      ),
+                    ),
+                    Container(
+                      height: MediaQuery.of(context).size.height * 0.35,
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.only(
+                              topLeft: Radius.circular(4),
+                              topRight: Radius.circular(4)),
+                          image: DecorationImage(
+                              image: AssetImage("images/1.jpeg"),
+                              fit: BoxFit.cover)),
+                    ),
+                    Container(
+                      margin: EdgeInsets.fromLTRB(
+                          20,
+                          50 + MediaQuery.of(context).size.height * 0.35,
+                          20,
+                          20),
+                      child: Center(
+                        child: Column(
+                          children: <Widget>[
+                            Text(
+                              "Beautiful Danau Di Sore Hari",
+                              maxLines: 2,
+                              textAlign: TextAlign.center,
+                              style: TextStyle(
+                                  color: Color(0xfff56d5d), fontSize: 25),
+                            ),
+                            Container(
+                              margin: EdgeInsets.fromLTRB(0, 20, 0, 15),
+                              child: Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: <Widget>[
+                                  Text(
+                                    "Posted On ",
+                                    style: TextStyle(
+                                        color: Colors.grey, fontSize: 12),
+                                  ),
+                                  Text(
+                                    "June 18, 2024",
+                                    style: TextStyle(
+                                        color: Color(0xfff56d5d), fontSize: 12),
+                                  ),
+                                ],
+                              ),
+                            ),
+                            Row(
+                              children: <Widget>[
+                                Spacer(
+                                  flex: 10,
+                                ),
+                                Icon(
+                                  Icons.thumb_up,
+                                  size: 18,
+                                  color: Colors.grey,
+                                ),
+                                Spacer(
+                                  flex: 1,
+                                ),
+                                Text(
+                                  "99",
+                                  style: TextStyle(color: Colors.grey),
+                                ),
+                                Spacer(
+                                  flex: 5,
+                                ),
+                                Icon(
+                                  Icons.comment,
+                                  size: 18,
+                                  color: Colors.grey,
+                                ),
+                                Spacer(
+                                  flex: 1,
+                                ),
+                                Text(
+                                  "888",
+                                  style: TextStyle(color: Colors.grey),
+                                ),
+                                Spacer(
+                                  flex: 10,
+                                )
+                              ],
+                            )
+                          ],
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              ),
             ),
+          ),
+        ],
+      ),
     );
-  }
-
-  List<Widget> get generateContainer {
-    return <Widget>[
-      Container(
-        color: Colors.red,
-        width: 100,
-        height: 100,
-      ),
-      Container(
-        color: Colors.blue,
-        width: 100,
-        height: 100,
-      ),
-      Container(
-        color: Colors.green,
-        width: 100,
-        height: 100,
-      ),
-    ];
   }
 }
