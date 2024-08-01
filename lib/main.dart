f3ee9a56afc143f8d6bc2da3b9181d4ceddce771
yabsiraditya <yabsir662@gmail.com>
2024-08-02 00:56:18 +0700

    day 7 learn-flutter InkWell Button

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,53 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: MainPage(),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Latihan Membuat button"),
+          backgroundColor: Colors.lime,
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: <Widget>[
+              ElevatedButton(
+                onPressed: () {},
+                child: Text("Elevated Button"),
+                style: ElevatedButton.styleFrom(backgroundColor: Colors.amber),
+              ),
+              Material(
+                elevation: 2,
+                borderRadius: BorderRadius.circular(20),
+                child: Container(
+                  width: 150,
+                  height: 40,
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20),
+                      gradient: LinearGradient(
+                          colors: [Colors.purple, Colors.pink],
+                          begin: Alignment.topCenter,
+                          end: Alignment.bottomCenter)),
+                  child: Material(
+                    borderRadius: BorderRadius.circular(20),
+                    color: Colors.transparent,
+                    child: InkWell(
+                      splashColor: Colors.amber,
+                      borderRadius: BorderRadius.circular(20),
+                      onTap: () {},
+                      child: Center(
+                          child: Text(
+                        "MyButton",
+                        style: TextStyle(
+                            color: Colors.white, fontWeight: FontWeight.w600),
+                      )),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
