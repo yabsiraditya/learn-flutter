14cbaccf96f273d06db2abc5d51b29b64698e230
yabsiraditya <yabsir.aditya@gmail.com>
2024-09-29 01:23:48 +0700

    update learn-flutter qrscan

---
+++ lib/fontFeature.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class FontFeature extends StatelessWidget {
+  const FontFeature({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+        title: Text("Flutter Typography"),
+        backgroundColor: Colors.lime,
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:qr_flutter/qr_flutter.dart';
-import 'package:qrscan/qrscan.dart' as scanner;
-import 'package:audioplayers/audioplayers.dart';
-import 'package:learn_flutter/newQr.dart';
+import 'package:learn_flutter/fontFeature.dart';
 
 void main() => runApp(MyApp());
 
@@ -11,7 +8,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Newqr(),
+      home: FontFeature(),
     );
   }
 }
